f02ac2b38e88075f006325e937fb2769c11ce221
Mahliyo <gasandigital13@gmail.com>
2026-04-07 11:58:56 +0500

    Json holatida ma'lumotlar

--- main.js
+++ main.js
@@ -1,5 +1,100 @@
 
 let container = document.getElementById("container");
-let cards = [
-    
+let data = [
+  {
+    "id": 1,
+    "ism": "Alisher",
+    "familiya": "Navoiy",
+    "yosh": 28,
+    "email": "alisher@example.uz",
+    "shahar": "Toshkent"
+  },
+  {
+    "id": 2,
+    "ism": "Zuhra",
+    "familiya": "Karimova",
+    "yosh": 24,
+    "email": "zuhra.k@example.uz",
+    "shahar": "Samarqand"
+  },
+  {
+    "id": 3,
+    "ism": "Bekzod",
+    "familiya": "Tursunov",
+    "yosh": 31,
+    "email": "bekzod31@example.uz",
+    "shahar": "Buxoro"
+  },
+  {
+    "id": 4,
+    "ism": "Laylo",
+    "familiya": "Usmonova",
+    "yosh": 22,
+    "email": "laylo.u@example.uz",
+    "shahar": "Farg'ona"
+  },
+  {
+    "id": 5,
+    "ism": "Jasur",
+    "familiya": "Aliyev",
+    "yosh": 35,
+    "email": "jasur_a@example.uz",
+    "shahar": "Namangan"
+  },
+  {
+    "id": 6,
+    "ism": "Madina",
+    "familiya": "Sodiqova",
+    "yosh": 27,
+    "email": "madina.s@example.uz",
+    "shahar": "Xiva"
+  },
+  {
+    "id": 7,
+    "ism": "Sardor",
+    "familiya": "Rashidov",
+    "yosh": 29,
+    "email": "sardor.r@example.uz",
+    "shahar": "Andijon"
+  },
+  {
+    "id": 8,
+    "ism": "Guli",
+    "familiya": "Ahmedova",
+    "yosh": 26,
+    "email": "guli.ah@example.uz",
+    "shahar": "Termiz"
+  },
+  {
+    "id": 9,
+    "ism": "Otabek",
+    "familiya": "Hoshimov",
+    "yosh": 33,
+    "email": "otabek.h@example.uz",
+    "shahar": "Jizzax"
+  },
+  {
+    "id": 10,
+    "ism": "Nigora",
+    "familiya": "Qosimova",
+    "yosh": 25,
+    "email": "nigora.q@example.uz",
+    "shahar": "Qarshi"
+  },
+  {
+    "id": 11,
+    "ism": "Rustam",
+    "familiya": "Murodov",
+    "yosh": 30,
+    "email": "rustam.m@example.uz",
+    "shahar": "Guliston"
+  },
+  {
+    "id": 12,
+    "ism": "Malika",
+    "familiya": "Ergasheva",
+    "yosh": 23,
+    "email": "malika.e@example.uz",
+    "shahar": "Nukus"
+  }
 ]
